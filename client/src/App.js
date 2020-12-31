33cbccb0a840b1c0209321b12df6f4b862f923b4
Shaun Tang <info@spcecycle.com.au>
2020-12-31 15:48:42 +1100

    changed my package json added client and server folder, see if the git will help me to upload both

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SPC eCycle Customer Service Mangement System</h1>
+      <p>a combination of CRM and service tracking system</p>
+
+      <div className="form">
+        <label>name: </label>
+        <input type="text" name="name"/>
+        <button>submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
